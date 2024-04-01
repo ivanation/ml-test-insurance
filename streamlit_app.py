e1678b53324683fd8840fdae5cc48d4dc3323fab
Ivan <71911343+ivanation@users.noreply.github.com>
2024-04-01 01:22:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,7 @@ prediction = predict_insurance_charge(input_data)
 
 st.header("Estimated Insurance Charge:")
 result_placeholder = st.empty()
-result_placeholder.write(prediction[0])
-valor_placeholder = st.subheader(result_placeholder)
-st.write(valor_placeholder)
+result_placeholder.write(st.subheader(prediction[0]))
 
 #####################
 
