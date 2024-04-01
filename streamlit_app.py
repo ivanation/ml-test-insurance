f96bcc2eb154e10c8d6e68bd8aa51ff6a7836519
Ivan <71911343+ivanation@users.noreply.github.com>
2024-04-01 00:27:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,13 +42,7 @@ st.subheader("Estimated Insurance Charge:")
 result_placeholder = st.empty()
 result_placeholder.write(prediction[0])
 
-"""
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#####################
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
